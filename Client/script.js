751bbb708535c34e0d13fac5e9ff16753b0e353b
Ayaan <kaifullah7921@gmail.com>
2022-07-08 22:05:27 +0530

    establishment of connection, handeling events, emitting events andsending  recieving of message is completed

--- Client/script.js
+++ Client/script.js
@@ -7,10 +7,30 @@ const messageContainer = document.querySelector("#messageContainer");
 const form = document.querySelector("#form");
 const socket = io("http://localhost:3000"); //backend connection port
 
+//  .on is the event listener or a method of socket.io liberary listening to "connect" event
+socket.on("connect", () => {
+  displayMessage(`client:: user connected with id ${socket.id}`);
+});
+
+// listening to send-message event, send-message is the event which is emitted by the server
+socket.on("send-message", (message) => {
+  displayMessage(message);
+  console.log(message);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+    const roomId = roomInput.value;
+    if(message === "") return
+  displayMessage(message);
+  socket.emit("message", message, roomId);
+  messageInput.value = "";
+});
 
 function displayMessage(message) {
-    const messageDiv = document.createElement("div");
-    messageDiv.className = "message";
-    messageDiv.textContent = message;
-    messageContainer.appendChild(messageDiv);
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message";
+  messageDiv.textContent = message;
+  messageContainer.appendChild(messageDiv);
 }
--- Server/Server.js
+++ Server/Server.js
@@ -1,1 +1,23 @@
-const io = require("socket.io")
+const io = require("socket.io")(3000, {
+    // backend connection port
+    // here if we keep it like this, then we'll get CORS errors,
+    // so we need to set the origins to allow all origins
+    cors: {
+        origin: "*", //allowing connection from all ports
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`server:: user connected with id ${socket.id}`);
+    socket.on("message", (message, roomId) => {
+        // message event is emitted by client whe a message is sent
+        // send-message is the event which is emitted by the server
+        if (roomId == "") {
+            socket.broadcast.emit("send-message", message);
+        } else {
+            socket.to(roomId).emit("send-message", message);
+        }
+        console.log(message);
+    }
+    );
+})
