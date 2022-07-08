7fd00220f6e8768f8559023ff4498d4c7dabda16
Ayaan <kaifullah7921@gmail.com>
2022-07-08 18:37:28 +0530

    variables, imports set up, function to display message is done

--- Client/script.js
+++ Client/script.js
@@ -1,1 +1,16 @@
 import { io } from "socket.io-client";
+const messageInput = document.querySelector("#messageInput");
+const roomInput = document.querySelector("#roomInput");
+const joinRoomButton = document.querySelector("#joinRoomButton");
+const leaveRoomButton = document.querySelector("#leaveRoomButton");
+const messageContainer = document.querySelector("#messageContainer");
+const form = document.querySelector("#form");
+const socket = io("http://localhost:3000"); //backend connection port
+
+
+function displayMessage(message) {
+    const messageDiv = document.createElement("div");
+    messageDiv.className = "message";
+    messageDiv.textContent = message;
+    messageContainer.appendChild(messageDiv);
+}
